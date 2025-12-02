e2952ea552b16b1aa581c9a68c0117a5b6a1dc4e
ullas <clubullas@gmail.com>
2025-12-02 19:37:39 +0530

    feat: add ci pipeline

---
+++ hello.js
@@ -0,0 +1,1 @@
+console.log("learning CI/CD with GitHub Actions!");
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("starting the test file!");
+setTimeout(()=>{
+    console.log("test file executed after 2 seconds");
+},2000);
+console.log("test file ended!");
